6c728b32361ed82a76925c081a0c7b664d7c7261
Sandhya417 <sanss89.krish@gmail.com>
2026-07-05 18:16:58 +0530

    Completed the 7 Django tutorials

--- Day12/djangotutorial/polls/admin.py
+++ Day12/djangotutorial/polls/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
+admin.site.site_header = "Polls Administration"
+admin.site.site_title = "Polls Admin"
+admin.site.index_title = "Welcome to Polls Administration"
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
--- Day12/djangotutorial/polls/views.py
+++ Day12/djangotutorial/polls/views.py
@@ -1,31 +1,39 @@
+from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.db.models import F
-from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
+from django.views import generic
 
 # Create your views here.
 
 
-def index(request):
-    latest_ques_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_ques_list": latest_ques_list}
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_ques_list"
 
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Exclude any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
